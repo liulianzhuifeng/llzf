7eb790aa05b9aa2277d8e4e7437e731d2240771d
liulianzhuifeng <liulianzhuifeng@outlook.com>
2013-11-19 20:39:29 +0800

    光电学院---洪鑫明

---
+++ js.js
@@ -0,0 +1,10 @@
+function getClassByClassName("site-nav-bd","nav-sec"){
+	var  site-nav-bd = document.getElementsByTagName("site-nav-bd");
+	var  site-nav-sec =[];
+	for(var i = 0; i<site-nav-bd.length;i++){
+	 if(site-nav-bd[i].className.indexOf(classname)){
+	 
+	 }
+	}
+	return site-nav-sec;
+}
