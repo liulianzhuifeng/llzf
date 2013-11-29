80e788cea76e448aa7950e7684a8b69f03672b30
liulianzhuifeng <liulianzhuifeng@outlook.com>
2013-11-29 19:53:37 +0800

    洪鑫明

---
+++ 光电学院——洪鑫明第五次作业/js/js.js
@@ -0,0 +1,6 @@
+
+    function showic(whichpic){
+        var source = whichpic.getAttribute("href");
+        var pla = document.getElementById("pla");
+        pla.setAttribute("src",source)
+    }
